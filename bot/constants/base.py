0a380491876e84dd162313187d91c008ef18916b
Adam <224856236+ad-pl@users.noreply.github.com>
2025-11-02 18:47:37 +0100

    Made base.py use pathlib

--- bot/constants/base.py
+++ bot/constants/base.py
@@ -11,24 +11,27 @@ contains the bot prefix and the extensions to load.
 
 from bot.utils import get_env_var
 from bot.constants.config import default_prefix
+from pathlib import Path,
+import re
+
 
 # CONSTANTS
 
 prefix = get_env_var("PREFIX", default=default_prefix, required=False, from_dot_env=True)
 
-# EXTENSIONS
+# this feels very cursed
 
-extensions = (
-  # fun fact: i tried to use pathlib for this.
-  #           i immediately had an aneurysm trying to understand
-  #           HOW to implement it.
-  #           so yeah, ig we hardcoding now. deal with it.
-  
-  "bot.cogs.ping",
-  "bot.cogs.help",
-  "bot.cogs.echo",
-  "bot.cogs.invite",
-  "bot.cogs.moderation.ban",
-  "bot.cogs.moderation.kick",
-  "bot.cogs.moderation.timeout",
-)
+f = list(Path('../cogs').iterdir())
+extensions = []
+for i in f:
+  if i.name == '__init__.py' or i.name == '__pycache__':
+    pass
+  elif i.is_file():
+    n = re.sub('[/\\\\]','.',re.sub('.py$','',str(i.relative_to('../cogs'))))
+    extensions.append(f"bot.cogs.{n}")
+  else:
+    for j in i.iterdir():
+      f.append(j)
+del f # i dont trust the garbage collector -ad
+      # (i actually just want 1-letter variables i will never write descriptive variables >:) )
+extensions = tuple(extensions)
