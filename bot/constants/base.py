b0584df644bd40683911d78cb6bf8592e3334cf3
_Spark <spark-aur@proton.me>
2025-11-03 16:34:56 +0300

    style: update base.py to make it more in-line with pre-existing styling

--- bot/constants/base.py
+++ bot/constants/base.py
@@ -7,34 +7,33 @@ contains the bot prefix and the extensions to load.
 
 # LIBRARIES AND MODULES
 
+from pathlib import Path
+import re # I HATE REGEX -spark
+
 ## pypkg
 
 from bot.utils import get_env_var
 from bot.constants.config import default_prefix
-from pathlib import Path
-import re
-
 
 # CONSTANTS
 
 prefix = get_env_var("PREFIX", default=default_prefix, required=False, from_dot_env=True)
 
-# this feels very cursed
+# ad: this feels very cursed
+# spark: it 100% is.
 
-f = list(Path('bot/cogs').iterdir())
+_cogs = list(Path('bot/_cogs').iterdir())
 extensions = []
-n = lambda i : re.sub('[/\\\\]','.',re.sub('.py$','',str(i.relative_to('bot/cogs'))))
+_regex = lambda i : re.sub('[/\\\\]','.',re.sub('.py$','',str(i.relative_to('bot/_cogs'))))
 
-for i in f:
-  if i.name == '__init__.py' or i.name == '__pycache__' or i.name == 'base.py':
-    pass
-  elif i.is_file():
-    #print(f"bot.cogs.{n(i)}")
-    extensions.append(f"bot.cogs.{n(i)}")
+for i in _cogs:
+  if i.name in ('__pycache__', '__init__.py', 'base.py'):
+    continue
+
+  if i.is_file():
+    extensions.append(f"bot._cogs.{_regex(i)}")
   else:
     for j in i.iterdir():
-      f.append(j)
+      _cogs.append(j)
 
-del f # i dont trust the garbage collector -ad
-del n # (i actually just want 1-letter variables i will never write descriptive variables >:) )
-extensions = tuple(extensions)
+extensions = tuple(extensions) # this looks extremely cursed. leaving it as a list would be fine. -spark
